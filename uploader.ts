4e047295ae1ecf51cc3c5ea6d4ce6592d75fddf8
NotBlizzard <furryblizzard@gmail.com>
2020-06-09 18:29:55 -0400

    fix upload folder path.

--- uploader.ts
+++ uploader.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, path.join(__dirname, "/uploads"));
+    cb(null, "uploads");
   },
   filename(req, file, cb) {
     const fileName = `${uuidv4()}${path.extname(file.originalname)}`;
